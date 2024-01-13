2231cd9f7eebbf290a9dc5250b51e26f21029681
Mike Cellini <mike.cellini@protonmail.com>
2024-01-12 22:13:17 -0600

    Enable basic input use case and data persistence

--- src/skate_log.py
+++ src/skate_log.py
@@ -1,5 +1,25 @@
 import argparse
 from datetime import date
+import sqlite3
+import os
+
+db_exists = False
+if os.path.isfile('skatelog.db'):
+    db_exists = True
+
+con = sqlite3.connect('skatelog.db')
+cur = con.cursor()
+
+if not db_exists:
+    cur.execute("CREATE TABLE person(name TEXT, birthdate TEXT)")
+    cur.execute("""CREATE TABLE activity(
+                        person TEXT,
+                        date TEXT,
+                        activity_type TEXT,
+                        skate TEXT,
+                        hours NUMERIC)""")
+    con.commit()
+
 
 parser = argparse.ArgumentParser(
                     prog='Skate Log',
@@ -8,9 +28,29 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-n', '--name')
 parser.add_argument('-d', '--date')
 parser.add_argument('-H', '--hours')
-parser.add_argument('-t', '--type')
+parser.add_argument('-s', '--skate')
+parser.add_argument('-t', '--activity_type')
 args = parser.parse_args()
 
 d = date.fromisoformat(args.date)
 
-print(f"{args.name} skated for {args.hours} hours on {d} at {args.type}")
+person_exists = False
+for row in cur.execute("SELECT name FROM person"):
+    if row[0] == args.name:
+        person_exists = True
+
+if not person_exists:
+    cur.execute("INSERT INTO person VALUES(?, ?)", (args.name, None))
+
+cur.execute("INSERT INTO activity VALUES(?, ?, ?, ?, ?)",
+            (
+                args.name,
+                args.date,
+                args.activity_type,
+                args.skate,
+                args.hours))
+con.commit()
+
+for row in cur.execute("""SELECT date, activity_type, skate, hours
+                          FROM activity"""):
+    print(f"""{args.name} skated for {row[3]} hours on {row[0]} at {row[1]}""")
